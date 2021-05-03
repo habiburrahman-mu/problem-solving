b754f0d549a12cf55b73652b069e23ce4d599430
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2021-05-04 00:46:15 +0600

    Create CF-96A.py

---
+++ CF-96A.py
@@ -0,0 +1,21 @@
+st = input()
+count = 1
+last_ch = st[0]
+for i in range(0, len(st)):
+	if(st[i]==last_ch):
+		count+=1
+		if(count>=7):
+			break
+	else:
+		count = 1
+		last_ch = st[i]
+
+# print(count)
+
+if(count>=7):
+	print('YES')
+else:
+	print('NO')
+
+
+#00000001
