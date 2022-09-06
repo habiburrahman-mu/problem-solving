5d3a48fae49e8a1d27316fa919b7e5c462a8b14e
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-09-06 17:50:38 +0600

    added leetcode solve

--- Leetcode/best-time-to-buy-and-sell-stock.ts
+++ Leetcode/best-time-to-buy-and-sell-stock.ts
@@ -19,5 +19,35 @@ function maxProfitBruteForceWithTLE(prices: number[]): number {
     return 0;
 };
 
+function maxProfitOneIteration(prices: number[]): number {
+    let minimumPrice = Number.MAX_SAFE_INTEGER;
+    let maximumProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minimumPrice) {
+            minimumPrice = prices[i];
+        }
+        let profit = prices[i] - minimumPrice;
+        if (profit > maximumProfit) {
+            maximumProfit = profit;
+        };
+    }
+    return maximumProfit;
+};
+
+function maxProfit(prices: number[]): number {
+    if (prices.length === 0)
+        return 0;
+
+    let currentMin = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        currentMin = prices[i] < currentMin ? prices[i] : currentMin;
+        maxProfit = prices[i] - currentMin > maxProfit ? prices[i] - currentMin : maxProfit;
+    }
+    
+    return maxProfit;
+};
+
 // let prices = [7,6,4,3,1];
 // console.log(maxProfit(prices));
