ddddf51b1c1c3cfdc61598ba6f655180de566f54
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-10-22 08:49:07 +0600

    Optimized leetcode 1009

--- Leetcode/complement-of-base-10-integer.cpp
+++ Leetcode/complement-of-base-10-integer.cpp
@@ -10,22 +10,42 @@ using namespace std;
 class Solution
 {
 public:
+    // int bitwiseComplement(int n)
+    // {
+    //     int i = 0;
+    //     int compDec = 0;
+    //     do
+    //     {
+    //         int bit = n & 1;
+    //         if (!bit)
+    //         {
+    //             compDec += pow(2, i);
+    //         }
+    //         i++;
+    //         n = n >> 1;
+    //     } while (n != 0);
+
+    //     return compDec;
+    // }
     int bitwiseComplement(int n)
     {
-        int i = 0;
-        int compDec = 0;
-        do
+        // let n = 5 -> 101;
+        int m = n, mask = 0;
+        if (m == 0)
         {
-            int bit = n & 1;
-            if (!bit)
-            {
-                compDec += pow(2, i);
-            }
-            i++;
-            n = n >> 1;
-        } while (n != 0);
+            return 1;
+        }
 
-        return compDec;
+        while (m != 0)
+        {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+
+        // mask would be 111
+
+        return ~n & mask; 
+        // 111111.....010 & 000000....111 = 00000....010
     }
 };
 
