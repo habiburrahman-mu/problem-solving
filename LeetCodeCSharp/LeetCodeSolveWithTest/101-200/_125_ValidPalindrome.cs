49a9ab805a9276c3c7e3ca837ad21c522feaa1d3
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-12-31 21:01:29 +0600

    test: added testcase for leetcode 125 ValidPalindrome

--- LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_125_ValidPalindrome.cs
+++ LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_125_ValidPalindrome.cs
@@ -2,5 +2,26 @@
 {
     public class _125_ValidPalindrome
     {
+        public bool IsPalindrome(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        [InlineData(" ", true)]
+
+        public void _125_ValidPalindrome_Test(string prices, bool expected)
+        {
+            // Arrange
+            var sut = MaxProfit;
+
+            // Act
+            var result = sut(prices);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
