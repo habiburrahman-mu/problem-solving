f9cef1aa392d97fd2018cfc0e6371f519f6470d7
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-12-31 20:22:20 +0600

    leetcode 121 best time to sell O(n2) solution

--- LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_121_BestTimeToBuyAndSellStock.cs
+++ LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_121_BestTimeToBuyAndSellStock.cs
@@ -1,22 +1,27 @@
-﻿using LeetCodeSolveWithTest._1_100;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeSolveWithTest._101_200
+﻿namespace LeetCodeSolveWithTest._101_200
 {
     public class _121_BestTimeToBuyAndSellStock
     {
         public int MaxProfit(int[] prices)
         {
-            throw new NotImplementedException();
+            List<int> profit = new List<int>();
+            for (int i = 0; i < prices.Length - 1; i++) // buy
+            {
+                for (int j = i + 1; j < prices.Length; j++) // sell
+                {
+                    profit.Add(prices[j] - prices[i]);
+                }
+            }
+
+            int max = profit.Count > 0 ? profit.Max() : 0;
+
+            return max > 0 ? max : 0;
         }
 
         [Theory]
         [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
         [InlineData(new[] { 7, 6, 4, 3, 1 }, 0)]
+        [InlineData(new int[] {  }, 0)]
         public void _121_BestTimeToBuyAndSellStock_Test(int[] prices, int expected)
         {
             // Arrange
