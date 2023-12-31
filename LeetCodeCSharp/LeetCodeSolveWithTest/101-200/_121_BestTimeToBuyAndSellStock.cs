9831b334bae7cb13bf9741519f7dd774060f3555
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-12-31 20:48:14 +0600

    solved: leetcode 121 BestTimeToBuyAndSellStock

--- LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_121_BestTimeToBuyAndSellStock.cs
+++ LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_121_BestTimeToBuyAndSellStock.cs
@@ -4,24 +4,26 @@
     {
         public int MaxProfit(int[] prices)
         {
-            List<int> profit = new List<int>();
-            for (int i = 0; i < prices.Length - 1; i++) // buy
+            int min = prices[0];
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
             {
-                for (int j = i + 1; j < prices.Length; j++) // sell
+                if (prices[i] > min)
                 {
-                    profit.Add(prices[j] - prices[i]);
+                    int profit = prices[i] - min;
+                    maxProfit = Math.Max(profit, maxProfit);
+                } else
+                {
+                    min = prices[i];
                 }
             }
 
-            int max = profit.Count > 0 ? profit.Max() : 0;
-
-            return max > 0 ? max : 0;
+            return maxProfit;
         }
 
         [Theory]
         [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
         [InlineData(new[] { 7, 6, 4, 3, 1 }, 0)]
-        [InlineData(new int[] {  }, 0)]
         public void _121_BestTimeToBuyAndSellStock_Test(int[] prices, int expected)
         {
             // Arrange
