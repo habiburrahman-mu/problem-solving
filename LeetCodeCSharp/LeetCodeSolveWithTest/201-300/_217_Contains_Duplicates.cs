556469e245b5b888aca6f4c70d868da0d90971d0
Habibur Rahman <habiburrahman.mu@gmail.com>
2024-08-20 23:21:55 +0600

    solved leetcode contains duplicates

---
+++ LeetCodeCSharp/LeetCodeSolveWithTest/201-300/_217_Contains_Duplicates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolveWithTest._201_300
+{
+    public class _217_Contains_Duplicates
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> hMap = new();
+            foreach (int num in nums)
+            {
+                if (hMap.ContainsKey(num))
+                    return true;
+                hMap.Add(num, 1);
+            }
+            return false;
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1 }, true)]
+        [InlineData(new[] { 1, 2, 3, 4 }, false)]
+        [InlineData(new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void _217_Contains_Duplicates_Tests(int[] nums, bool expected)
+        {
+            // Arrange
+            var sut = ContainsDuplicate;
+
+            // Act
+            var result = sut(nums);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
