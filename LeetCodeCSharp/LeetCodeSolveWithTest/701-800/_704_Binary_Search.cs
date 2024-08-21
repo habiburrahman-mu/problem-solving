06d81afba85af18d223a30aff0e0c05db3a3a72a
Habibur Rahman <habiburrahman.mu@gmail.com>
2024-08-21 23:35:05 +0600

    solved overflow issue in _704_Binary_Search

--- LeetCodeCSharp/LeetCodeSolveWithTest/701-800/_704_Binary_Search.cs
+++ LeetCodeCSharp/LeetCodeSolveWithTest/701-800/_704_Binary_Search.cs
@@ -5,12 +5,30 @@
     {
         public int Search(int[] nums, int target)
         {
-            return 0;
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + ((right - left) / 2); // (left + right) / 2 can lead to overflow
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
         }
 
         [Theory]
         [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
         [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
+        [InlineData(new[] { 5 }, 5, 0)]
         public void _704_Binary_Search_Test(int[] nums, int target, int expected)
         {
             // Arrange
