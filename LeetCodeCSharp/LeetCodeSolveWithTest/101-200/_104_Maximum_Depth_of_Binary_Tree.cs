822f77068dd1bc07ab3c89c91bfb695a8e4f6639
Habibur Rahman <habiburrahman.mu@gmail.com>
2024-08-25 13:46:32 +0600

    added test cases for leetcode 104. Maximum Depth of Binary Tree
    https://leetcode.com/problems/maximum-depth-of-binary-tree

---
+++ LeetCodeCSharp/LeetCodeSolveWithTest/101-200/_104_Maximum_Depth_of_Binary_Tree.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCodeSolveWithTest._101_200
+{
+    public class _104_Maximum_Depth_of_Binary_Tree
+    {
+        private class TreeNode
+        {
+            public int val;
+            public TreeNode? left;
+            public TreeNode? right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private int MaxDepth(TreeNode root)
+        {
+            return 0;
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, 3 };
+            yield return new object[] { new int?[] { 1, null, 2 }, 2 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void _104_Maximum_Depth_of_Binary_Tree_Test(int?[] values, int expected)
+        {
+            // Arrange
+            var sut = MaxDepth;
+            TreeNode? root = CreateTreeFromLevelOrder(values);
+
+            // Act
+            var result = sut(root);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        private static TreeNode? CreateTreeFromLevelOrder(int?[] array, int startIndex = 0)
+        {
+            if (array.Length == 0 && array[startIndex].HasValue == false)
+                return null;
+            var value = array[startIndex];
+            if (value == null)
+                return null;
+
+            var currentNode = new TreeNode(value.Value);
+
+            var leftIndex = 2 * startIndex + 1;
+            var rightIndex = 2 * startIndex + 2;
+
+            if (leftIndex < array.Length)
+            {
+                currentNode.left = CreateTreeFromLevelOrder(array, leftIndex);
+            }
+
+            if (rightIndex < array.Length)
+            {
+                currentNode.right = CreateTreeFromLevelOrder(array, rightIndex);
+            }
+
+            return currentNode;
+        }
+    }
+}
